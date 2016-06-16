aa43f0a62d6f438bae0c3756a7a355d374454902
hugo <HUGOPKU@QQ.COM>
2016-06-16 11:23:39 +0800

    SDK ok

--- src/main/java/com/hisign/netty/worker/HisignBVESDK.java
+++ src/main/java/com/hisign/netty/worker/HisignBVESDK.java
@@ -1,5 +1,9 @@
 package com.hisign.netty.worker;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +14,19 @@ import com.hisign.thid.Point;
 import com.hisign.thid.THIDFaceSDK;
 
 public class HisignBVESDK {
+	
+	static String fileName1 = "F:\\1.jpg";
+	static String fileName2 = "F:\\2.jpg";
+	
+	public static void main(String[] args) throws IOException {
+		int init = THIDFaceSDK.init(null, null, null);
+		System.out.println(init);
+		
+		byte[] i1 = getTemplateByImageByteArray(readFile(new File(fileName1)));
+		byte[] i2 = getTemplateByImageByteArray(readFile(new File(fileName2)));
+		System.out.println(compareFromTwoTemplate(i1, i2));
+		
+	}
 	
 	public static float compareFromTwoImages(byte[] img1, byte[] img2){
 		
@@ -33,7 +50,19 @@ public class HisignBVESDK {
 		return score;
 	}
 	
+	public static byte[] readFile(File file) throws IOException {
+        DataInputStream dis = new DataInputStream(new FileInputStream(file));
+        byte[] bs = new byte[(int) file.length()];
+        dis.readFully(bs);
+        dis.close();
+        return bs;
+    }
 	
+	/**
+	 * 获取模板数据
+	 * @param img
+	 * @return
+	 */
 	public static byte[] getTemplateByImageByteArray(byte[] img) {
 		
 		GrayImg grayimg = new GrayImg();
@@ -45,19 +74,19 @@ public class HisignBVESDK {
 		//获取脸部数据
 		List<FacePos> facePoses = new ArrayList<FacePos>();
         int detect = THIDFaceSDK.detect(grayimg, null, null, 1, facePoses);
-        System.out.println("detect返回值：" + detect);
+//        System.out.println("detect返回值：" + detect);
         EyePos eyepos = new EyePos();
         int locate = 0;
         if (facePoses.size() > 0) {
         	locate = THIDFaceSDK.locate(grayimg, facePoses.get(0).rect, eyepos);
         }
-        System.out.println("locate返回值：" + locate);
+//        System.out.println("locate返回值：" + locate);
         Point[] points = new Point[88];
         int align = 0;
         if (facePoses.size() > 0 && eyepos.left != null & eyepos.right != null) {
            align = THIDFaceSDK.align(grayimg, eyepos, points);
         }
-        System.out.println("align返回值：" + align);
+//        System.out.println("align返回值：" + align);
         
         //获取模板数据
         if (facePoses.size() > 0 && eyepos.left != null && eyepos.right != null) {
